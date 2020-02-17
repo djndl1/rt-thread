04f4ee2b8314042f48ca396279aec9b3a4e9e99b
djndl1 <djndl1@gmail.com>
2020-02-17 21:50:08 +0800

    thread header finished

--- components/cplusplus/cpp_thread.cpp
+++ components/cplusplus/cpp_thread.cpp
@@ -7,6 +7,37 @@
 namespace std
 {
 
+    static void* execute_native_thread_routine(void *p)
+    {
+        thread::invoker_base* t = static_cast<thread::invoker_base*>(p);
+        thread::invoker_base_ptr local;
+        local.swap(t->this_ptr); // tranfer the ownership of the invoker into the thread entry
+
+        local->invoke();
+
+        return NULL;
+    }
+
+    void thread::start_thread(invoker_base_ptr b)
+    {
+        auto raw_ptr = b.get();
+        // transfer the ownership of the invoker to the new thread
+        raw_ptr->this_ptr = std::move(b); 
+        int err = pthread_create(&_m_thr.__cpp_thread_t, NULL,
+                &execute_native_thread_routine, raw_ptr);
+
+        if (err) 
+        {
+            raw_ptr->this_ptr.reset();
+        #ifdef RT_USING_CPP_EXCEPTION
+            throw system_error(error_code(err, system_category()), "Failed to create a thread");
+        #else
+            abort();
+        #endif
+        }
+        
+    }    
+
     thread::~thread()
     {
         if (joinable()) // when either not joined or not detached
@@ -17,14 +48,18 @@ namespace std
     {
         int err = EINVAL;
 
-        if (joinable())
-            err = pthread_join(_m_thr.__cpp_thread_t, NULL);
-        if (err)
-    #ifdef RT_USING_CPP_EXCEPTION
+      //  if (joinable())
+            err = pthread_join(native_handle(), NULL);
+        
+        if (err) 
+        {
+        #ifdef RT_USING_CPP_EXCEPTION
             throw system_error(error_code(err, system_category()), "thread::join failed");
-    #else
+        #else
             abort();
-    #endif
+        #endif
+        }
+    
         _m_thr = id();
     }
 
@@ -33,14 +68,17 @@ namespace std
         int err = EINVAL;
 
         if (joinable())
-            err = pthread_detach(_m_thr.__cpp_thread_t);
+            err = pthread_detach(native_handle());
         if (err)
-    #ifdef RT_USING_CPP_EXCEPTION
+        {
+        #ifdef RT_USING_CPP_EXCEPTION
             throw system_error(error_code(err, system_category()), "thread::detach failed");
-    #else
+        #else
             abort();
-    #endif
-        _m_thr = id();            
+        #endif
+        }
+    
+        _m_thr = id();
     }
 
 // TODO: not yet actually implemented.

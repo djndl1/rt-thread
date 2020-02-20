2c2ca304ed9bfe8c7e039b7c144f60a9d9372c9b
djndl1 <djndl1@gmail.com>
2020-02-20 18:14:07 +0800

    condition_variable implemented

---
+++ components/cplusplus/cpp_condition_variable.cpp
@@ -0,0 +1,15 @@
+#include "condition_variable"
+
+namespace std
+{
+    void condition_variable::wait(unique_lock<mutex>& lock)
+    {
+        int err = pthread_cond_wait(&_m_cond, lock.mutex()->native_handle());
+
+        if (err)
+        {
+            throw_system_error(err, "condition_variable::wait: failed to wait on a condition");
+        }
+    }
+
+} // namespace std
